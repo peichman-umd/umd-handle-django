79e2dad178d604bf00a5644bba92a1068be8326b
David P. Steelman <dsteelma@umd.edu>
2025-11-05 07:43:57 -0500

    LIBITD-2592. Initial project setup
    Initial setup of the "umd-handle-django" application following the
    steps in https://umd-dit.atlassian.net/wiki/x/CAD8Ug
    
    Used "umd_handle" for the project name, because using "umd-handle"
    is not allowed (returned the following error):
    
    ```
    CommandError: 'umd-handle' is not a valid project name. Please make sure the name is a valid identifier.
    ```
    
    https://umd-dit.atlassian.net/browse/LIBITD-2592

---
+++ src/umd_handle/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for umd_handle project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'umd_handle.settings')
+
+application = get_asgi_application()
