26a24f71f2c1649b0e1afd32c8a7eca2a31190ac
David P. Steelman <dsteelma@umd.edu>
2025-11-13 08:28:23 -0500

    LIBITD-2606. Limit Handle model to list of allowed prefixes
    Modified the "prefix" field in the "Handle" model to only allow a
    list of allowed prefixes. Add validator to verify allowed prefixes
    before saving the model.
    
    Added database migration to support the allowed prefixes.
    
    Added unit test to verify.
    
    https://umd-dit.atlassian.net/browse/LIBITD-2606

--- src/umd_handle/api/models.py
+++ src/umd_handle/api/models.py
@@ -1,8 +1,21 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
 
+def validate_prefix(value):
+    if value not in Handle.ALLOWED_PREFIXES:
+        raise ValidationError(f"'{value}' is not an allowed prefix.")
+
 class Handle(TimeStampedModel):
-    prefix = models.CharField()
+
+    ALLOWED_PREFIXES = [
+        '1903.1',
+    ]
+
+    prefix = models.CharField(
+        choices=[(prefix, prefix) for prefix in ALLOWED_PREFIXES],
+        validators=[validate_prefix]
+    )
     suffix = models.IntegerField()
     url = models.CharField()
     repo = models.CharField()
---
+++ tests/umd_handle/handle_validation_tests.py
@@ -0,0 +1,21 @@
+import pytest
+from django.core.exceptions import ValidationError
+from umd_handle.api.models import Handle
+
+@pytest.fixture
+def valid_handle():
+    return Handle(
+        prefix='1903.1',
+        suffix=1,
+        url='http://example.com/valid-handle',
+        repo='avalon',
+        repo_id='avalon-test'
+    )
+
+@pytest.mark.django_db
+def test_prefix_must_be_known_prefix(valid_handle):
+    handle = valid_handle
+    handle.prefix = 'INVALID_PREFIX'
+
+    with pytest.raises(ValidationError):
+        handle.full_clean()
