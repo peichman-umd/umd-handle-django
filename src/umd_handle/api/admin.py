88ca23329a82457ea84a746bc214f1d9c9a6e73c
David P. Steelman <dsteelma@umd.edu>
2025-11-19 11:54:37 -0500

    LIBITD-2612. Modify "Handle" sorting to treat suffixes numerically
    Added a "_handle_sort_key" to the HandleAdmin model queryset so that
    the concatenated "Handle" field, when sorting, left-pads zeroes onto
    the "suffix" field, so that, when sorted alphabetically, the suffixes
    will appear in numeric order.
    
    Co-authored-by: Google "AI Mode"
    https://umd-dit.atlassian.net/browse/LIBITD-2612

--- src/umd_handle/api/admin.py
+++ src/umd_handle/api/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
-from django.db.models import Value
-from django.db.models.functions import Concat
+from django.db.models import CharField, Value
+from django.db.models.functions import Cast, Concat, LPad
 from django.utils.html import format_html
 
 from .models import Handle
@@ -34,13 +34,29 @@ class HandleAdmin(admin.ModelAdmin):
         'prefix', 'suffix', 'url', 'repo', 'repo_id', 'description', 'notes'
     ]
 
+    def get_queryset(self, request):
+      queryset = super().get_queryset(request)
+
+      # Modify the the "Handle" (prefix/suffix) combination sorting to ensure
+      # that the suffixes are sorted numerically by left-padding with zero
+      # out to 10 digits (the approximate max length of the IntegerField).
+      return queryset.annotate(
+          _handle_sort_key=Concat(
+              'prefix',
+              # Pad the string representation of suffix with up to 10 leading zeros
+              LPad(Cast('suffix', output_field=CharField()), Value(10), Value('0')),
+              output_field=CharField()
+          )
+      )
+
+
     list_filter = ('repo', 'created', 'modified')
 
     @admin.display(description='URL', ordering='url')
     def url_link(self, obj):
         return format_html('<a href="{url}">{url}</a>', url=obj.url)
 
-    @admin.display(description='Handle', ordering=Concat('prefix', Value(' '), 'suffix'))
+    @admin.display(description='Handle', ordering='_handle_sort_key')
     def combined_handle(self, obj):
         return str(obj)
 
